7c0fa3b09ce32eb0ecc7d0d8eed58d69c31d0549
AmirReza <amir22jahani22@gmail.com>
2024-11-10 10:45:03 -0800

    F- Has been added

---
+++ F/F-app-2.js
@@ -0,0 +1,93 @@
+let app = require('./F-http-2.js');
+let fs = require('fs');
+
+app.use('/sum', function(request, response){
+    result = {
+        data: parseInt(request.path[2]) + parseInt(request.path[3])
+    };
+    write(response, result);
+});
+app.use('/multiply', function(request, response){
+    result = {
+        data: parseInt(request.path[2]) * parseInt(request.path[3])
+    };
+    write(response, result);
+});
+app.use('/printRecord', function(request, response){
+    result = {
+        "name": request.path[2],
+        "family": request.path[3],
+        "email": request.path[4]
+    }
+    write(response, result);
+});
+
+app.use('/saveRecord', function(request, response){
+    result = {
+        "name": request.path[2],
+        "family": request.path[3],
+        "email": request.path[4]
+    }
+    writeFile('database.txt', result, function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved.'});
+    });
+});
+
+app.use('/create', function(request, response){
+    result = {
+        "name": request.path[3],
+        "family": request.path[4],
+        "email": request.path[5]
+    }
+    writeFile(request.path[2], result, function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved.'});
+    });
+});
+
+app.use('/file', function(request, response){
+    myContent = request.data;
+    console.log("My data: ", myContent);
+    
+    writeFilePost(request.data.name , request.data , function(){
+        console.log('File Saved.');
+        write(response, { result: 'File Saved.'});
+    });
+    
+});
+
+
+function writeFilePost(name, body, callback){
+    body = JSON.stringify(body);
+    fs.writeFile(name, body, function(error){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, { result: 'ERROR:', error});
+        }
+        else{
+            callback();
+        }
+    })
+}
+
+function write(res, body){
+    res.write(JSON.stringify(body));
+    res.end();
+}
+
+function writeFile(name, body, callback){
+    body = JSON.stringify(body);
+    fs.writeFile(name, body, function(error){
+        if(error){
+            console.log('ERROR:', error);
+            write(response, { result: 'ERROR:', error});
+        }
+        else{
+            callback();
+        }
+    })
+}
+
+
+app.start();
